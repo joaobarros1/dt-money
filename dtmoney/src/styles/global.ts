d996bf67e607fd43b69a203179e190576f9eb30f
Joao Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-03-25 17:40:41 -0300

    global-css

--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
     --red: #E52E4D;
+    --green: #33CC95;
     --blue: #5429CC;
     
     --blue-light: #6933FF;
@@ -34,6 +35,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
